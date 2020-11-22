3d3c3031b7c13f81bbf1ca732b45fd4ce02a8736
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-11-22 13:18:31 -0700

    first commit Test

---
+++ src/test/java/day1/TestConnection.java
@@ -0,0 +1,19 @@
+package day1;
+
+import java.sql.*;
+
+public class TestConnection {
+
+    public static void main(String[] args) throws SQLException {
+        // REPLACE THIS IP ADDRESS WITH YOUR OWN THAT WORKING IN SQL DEVELOPER
+        String connectionStr = "jdbc:oracle:thin:@3.86.188.174:1521:XE";
+        String username = "hr" ;
+        String password = "hr" ;
+        Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
+        Statement stmnt = conn.createStatement();
+        ResultSet rs   =   stmnt.executeQuery("SELECT * FROM REGIONS") ;
+        rs.next(); // currently we are at the very first row
+        System.out.println("first column value using index: --> " +  rs.getString(1)   );
+    }
+}
+//clear and test
