df55200b04c6234c8f973ad2475495cd048bbeac
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-11-24 18:57:35 -0700

    first commit Test 4

--- src/test/java/day2/GettingMoreInfoAboutResultSetObject.java
+++ src/test/java/day2/GettingMoreInfoAboutResultSetObject.java
@@ -5,16 +5,42 @@ import java.sql.*;
 public class GettingMoreInfoAboutResultSetObject {
 
     public static void main(String[] args) throws SQLException {
+
         String connectionStr = "jdbc:oracle:thin:@3.86.188.174:1521:XE";
         String username = "hr" ;
         String password = "hr" ;
 
         Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
-        // this way of creating statement will give you ability to generate
-        // ResultSet that can move forward and backward anytime
-        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         ResultSet rs   =   stmt.executeQuery("SELECT * FROM JOBS") ;
+        //ResultSet rs   =   stmt.executeQuery("SELECT * FROM EMPLOYEES") ;
 
+        // Metadata -- data about the data --
+        // ResultSetMetaData -- data about the ResultSet object that contain our resulting rows and columns
+        //   for example column names , column counts .. and more
+        // This is how we get the ResultSetMetaData object
+
+        // ResultSetMetaData itself does not contains any row data
+        // it only has information about the columns
+        ResultSetMetaData rsmd = rs.getMetaData();
+        // we only need two methods from this to get column count and column name | label
+
+        int colCount = rsmd.getColumnCount();
+        System.out.println("colCount = " + colCount);
+        // this is how we get column label | name using index
+        // get first column name
+        System.out.println("First Column Name is " + rsmd.getColumnLabel(1)  );
+        System.out.println("Second Column Name is " + rsmd.getColumnLabel(2)  );
+
+        // now print out all column names :
+
+        for (int colNum = 1; colNum <= colCount ; colNum++) {
+            System.out.println("Column name " + rsmd.getColumnLabel(colNum)   );
+        }
+
+        rs.close();
+        stmt.close();
+        conn.close();
 
 
     }
